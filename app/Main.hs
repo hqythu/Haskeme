01ece78259c00fe11f8aab5fc68d4d6f737e0f14
何钦尧 <hqythu@gmail.com>
2017-01-15 02:41:21 +0800

    parse integer and float

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,9 @@
 module Main where
 
+import System.Environment
 import Lib
 
 main :: IO ()
-main = someFunc
+main = do 
+    (expr:_) <- getArgs
+    putStrLn (parseExpr expr)
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,28 @@
 module Lib
-    ( someFunc
+    ( parseExpr
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Text.ParserCombinators.Parsec hiding (spaces)
+import Control.Monad
+
+data SchemeVal 
+    = Bool Bool
+    | Number Double
+
+parseInteger :: Parser SchemeVal
+parseInteger = liftM (Number . \x -> read x :: Double) (many1 digit)
+
+parseDouble :: Parser SchemeVal
+parseDouble = do
+    x <- many1 digit
+    char '.'
+    y <- many1 digit
+    return $ Number (read (x ++ ['.'] ++ y) :: Double)
+
+parseNumber :: Parser SchemeVal
+parseNumber = try parseDouble <|> parseInteger
+
+parseExpr :: String -> String
+parseExpr input = case parse parseNumber "lisp" input of
+    Left err -> "No match: " ++ show err
+    Right (Number x) -> show x
